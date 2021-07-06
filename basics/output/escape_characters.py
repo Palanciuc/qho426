ac344048472d46d97791e900bf8c42073e4c93d5
PalanciucDragos <>
2021-07-06 10:49:16 +0000

    Updated code to display a message with escape characters.

--- basics/output/escape_characters.py
+++ basics/output/escape_characters.py
@@ -1,6 +1,6 @@
 # Display escape characters
-print("\n Displays a new line")
-print("\t Displays a tab space")
-print("\\ Displays a back slash")
-print("\" Displays a double quotes")
-print("\' Displays a single quote")
+print("\n")
+print("\n")
+print("\t\"I am Beep!\"")
+print("\n")
+print("\n")
